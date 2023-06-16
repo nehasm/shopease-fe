5b518d302aee123474d5fe8a7c52436e654757f3
neha-malvia <neha.malvia@freshworks.com>
2023-06-17 02:28:41 +0530

    added order screen

--- src/component/cart/ordereditem.js
+++ src/component/cart/ordereditem.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from 'react';
+import style from './order.module.css';
+import { AiFillStar } from 'react-icons/ai';
+
 
 const OrderedItem = (props) => {
+  const date = new Date(props.order.createdAt)
   return <>
-    {props.order.orderItems.map(product => <div>
-        {product.name}
+    {props.order.orderItems.map(product => <div className={style.orderitem}>
+          <div className={style.productimg}>
+        <span className={style["product-card-img"]} style={{backgroundImage: `url(${product.image})`}}>
+            </span>
+        </div>
+        <div className={style.orderitemtextmain}>
+        <div >
+            <span className={style.cartitemtitle}>{product.name}</span>
+        <div className={style.cartitemprice}>
+        <span>{`₹${product.price}`}</span>
+        </div>
+        <div className={style.orderdate}>
+          {`Ordered at : ${date.getDate()} ${date.toLocaleString('default', { month: 'long' })}, ${date.getFullYear()}`}
+        </div>
+        <div className={style.textcolorhighlight}>
+          {`${props.order.orderStatus} your Order`}
+        </div>
+        {
+          props.order.orderStatus === "Delivered" &&
+          <div className={style.reviewproduct}>
+          <AiFillStar />  <span>Rate & Review Product</span>
+        </div>
+        }
+
+        </div>
+    </div>
     </div>)}
     </>
-  
 }
 
 export default OrderedItem
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,16 +4,37 @@ import Loader from '../component/common/loader';
 import OrderedItem from '../component/cart/ordereditem';
 import { useDispatch } from 'react-redux';
 import { getAllOrders } from '../service/order';
+import style from '../component/cart/order.module.css';
+import { useNavigate } from 'react-router-dom';
+import icon from '../assets/icon.png';
+import {BsArrowLeft} from 'react-icons/bs';
+
 
 const Orders = () => {
     const {orders,error,loading} = useSelector(state => state.order);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     useEffect(()=>{
         dispatch(getAllOrders())
     },[])
-  return <>
-  {loading ? <Loader/> : orders.map(order => <OrderedItem order={order}/>)}
-  </>
+    const moveToHomePage = () => {
+      return navigate('/');
+    }
+  return <div>
+    <div className={style.cartheader}>
+      <BsArrowLeft onClick={moveToHomePage}/>
+      <span >
+      <img  src={icon} alt='emall' />
+      </span>
+    </div>
+    <div className={style.ordermain}>
+  {loading ? <Loader/> : <div>
+    {orders.map(order => <OrderedItem order={order}/>)}
+  </div>}
+  </div>
+  </div>
+  
+
 }
 
 export default Orders
